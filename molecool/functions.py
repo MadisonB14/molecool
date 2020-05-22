6774f821e95f337751992a46d0ef9810c4b61cac
Madison Berger <madisonberger14@hotmail.com>
2020-05-22 10:02:39 -0500

    fix imports

--- molecool/functions.py
+++ molecool/functions.py
@@ -4,6 +4,15 @@ A Python package for analyzing and visualizing pdb and xyz files. For MolSSI May
 
 Handles the primary functions
 """
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+
+%matplotlib notebook
+
 def calculate_distance(rA, rB):
     # This function calculates the distance between two points given as numpy arrays.
     d=(rA-rB)
@@ -23,29 +32,6 @@ def open_pdb(f_loc):
             c.append(c2)
     coords = np.array(c)
     return sym, coords
-
-atomic_weights = {
-    'H': 1.00784,
-    'C': 12.0107,
-    'N': 14.0067,
-    'O': 15.999,
-    'P': 30.973762,
-    'F': 18.998403,
-    'Cl': 35.453,
-    'Br': 79.904,
-}
-
-atom_colors = {
-    'H': 'white',
-    'C': 'HD3D3D3',
-    'N': '#add8e6',
-    'O': 'red',
-    'P': '#FFA500',
-    'F': '#FFFFE0',
-    'Cl': '#98FB98',
-    'Br': '#F4A460',
-    'S': 'yellow'
-}
 
 def open_xyz(file_location):
 
